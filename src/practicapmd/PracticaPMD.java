c2abca459854741180e5d1200546f794ccbb9271
jesus <jesus@DESKTOP-S3K1E1G>
2024-01-05 20:54:04 +0100

    Carga inicial del proyecto

---
+++ src/practicapmd/PracticaPMD.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicapmd;
+
+public class PracticaPMD {
+
+    public static void main(String[] args) {
+        int a;
+        
+        int altura = 89;
+        if (altura > 89) {
+            System.out.println("Eres muy alto");
+        }
+        PracticaPMD ejemPMD = new PracticaPMD();
+        ejemPMD.MetodoDobleReturn();
+        metodo();
+    }
+
+    static void metodo() {
+
+    }
+    
+    static void metodo2() {
+
+    }
+static void metodo3() {
+
+    }
+    public boolean MetodoDobleReturn() {
+        int iValor = 10;
+        if (iValor == 5) 
+            return false;
+         else {
+            return true;
+        }
+    }
+
+}
